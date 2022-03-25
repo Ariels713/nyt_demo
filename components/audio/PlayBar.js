6b170785cecf022df48ff9192a2a724c0bf90c84
Ariels713 <Ariels713@gmail.com>
2022-03-25 08:48:57 -0400

    fixing grid

--- components/audio/PlayBar.js
+++ components/audio/PlayBar.js
@@ -1,85 +1,86 @@
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes } from 'styled-components'
 
 function PlayBar() {
   return (
     <Wrapper>
-      <Rewind xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+      <Rewind xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'>
         <path
-          fill="var(--color-primary)"
-          fillrule="evenodd"
-          cliprule="evenodd"
-          d="M14.989 4.16232V0L4.98901 6H13.0714V6.00104C19.109 6.08286 23.978 10.9759 23.978 17C23.978 23.0751 19.0261 28 12.9176 28C7.4959 28 2.98529 24.1202 2.03961 19H0C0.967285 25.2302 6.38239 30 12.9176 30C20.1368 30 25.989 24.1797 25.989 17C25.989 10.5213 21.2238 5.14957 14.989 4.16232Z"
-          transform="translate(2)"
+          fill='var(--color-primary)'
+          fillrule='evenodd'
+          cliprule='evenodd'
+          d='M14.989 4.16232V0L4.98901 6H13.0714V6.00104C19.109 6.08286 23.978 10.9759 23.978 17C23.978 23.0751 19.0261 28 12.9176 28C7.4959 28 2.98529 24.1202 2.03961 19H0C0.967285 25.2302 6.38239 30 12.9176 30C20.1368 30 25.989 24.1797 25.989 17C25.989 10.5213 21.2238 5.14957 14.989 4.16232Z'
+          transform='translate(2)'
         ></path>
         <path
-          fill="var(--color-primary)"
-          fillrule="evenodd"
-          cliprule="evenodd"
-          d="M3.476 7.821V0.121H0.363L-1.79291e-07 1.683H1.54V7.821H3.476ZM8.92 7.942C6.951 7.942 5.785 6.347 5.785 3.971C5.785 1.584 6.951 -9.91821e-08 8.92 -9.91821e-08C10.878 -9.91821e-08 12.022 1.584 12.022 3.971C12.022 6.347 10.878 7.942 8.92 7.942ZM8.92 6.281C9.624 6.281 10.086 5.533 10.086 3.971C10.086 2.42 9.624 1.661 8.92 1.661C8.216 1.661 7.743 2.42 7.743 3.971C7.743 5.533 8.216 6.281 8.92 6.281Z"
-          transform="translate(9.07812 13.179)"
+          fill='var(--color-primary)'
+          fillrule='evenodd'
+          cliprule='evenodd'
+          d='M3.476 7.821V0.121H0.363L-1.79291e-07 1.683H1.54V7.821H3.476ZM8.92 7.942C6.951 7.942 5.785 6.347 5.785 3.971C5.785 1.584 6.951 -9.91821e-08 8.92 -9.91821e-08C10.878 -9.91821e-08 12.022 1.584 12.022 3.971C12.022 6.347 10.878 7.942 8.92 7.942ZM8.92 6.281C9.624 6.281 10.086 5.533 10.086 3.971C10.086 2.42 9.624 1.661 8.92 1.661C8.216 1.661 7.743 2.42 7.743 3.971C7.743 5.533 8.216 6.281 8.92 6.281Z'
+          transform='translate(9.07812 13.179)'
         ></path>
       </Rewind>
       <Play
-        fill="var(--color-primary)"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 40 40"
-        class="play-triangle-3-_pUfBt"
+        fill='var(--color-primary)'
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 40 40'
+        class='play-triangle-3-_pUfBt'
       >
         <path
-          fill="var(--color-primary)"
-          fillrule="evenodd"
-          cliprule="evenodd"
-          d="M0 40V0L33.6 20L0 40Z"
-          transform="translate(3)"
+          fill='var(--color-primary)'
+          fillrule='evenodd'
+          cliprule='evenodd'
+          d='M0 40V0L33.6 20L0 40Z'
+          transform='translate(3)'
         ></path>
       </Play>
-      <FastForward xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+      <FastForward xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'>
         <path
-          fill="var(--color-primary)"
-          fillrule="evenodd"
-          cliprule="evenodd"
-          d="M14.989 4.16232V0L4.98901 6H13.0714V6.00104C19.109 6.08286 23.978 10.9759 23.978 17C23.978 23.0751 19.0261 28 12.9176 28C7.4959 28 2.98529 24.1202 2.03961 19H0C0.967285 25.2302 6.38239 30 12.9176 30C20.1368 30 25.989 24.1797 25.989 17C25.989 10.5213 21.2238 5.14957 14.989 4.16232Z"
-          transform="translate(27.989) scale(-1 1)"
+          fill='var(--color-primary)'
+          fillrule='evenodd'
+          cliprule='evenodd'
+          d='M14.989 4.16232V0L4.98901 6H13.0714V6.00104C19.109 6.08286 23.978 10.9759 23.978 17C23.978 23.0751 19.0261 28 12.9176 28C7.4959 28 2.98529 24.1202 2.03961 19H0C0.967285 25.2302 6.38239 30 12.9176 30C20.1368 30 25.989 24.1797 25.989 17C25.989 10.5213 21.2238 5.14957 14.989 4.16232Z'
+          transform='translate(27.989) scale(-1 1)'
         ></path>
         <path
-          fill="var(--color-primary)"
-          fillrule="evenodd"
-          cliprule="evenodd"
-          d="M3.476 7.821V0.121H0.363L-1.79291e-07 1.683H1.54V7.821H3.476ZM8.92 7.942C6.951 7.942 5.785 6.347 5.785 3.971C5.785 1.584 6.951 -9.91821e-08 8.92 -9.91821e-08C10.878 -9.91821e-08 12.022 1.584 12.022 3.971C12.022 6.347 10.878 7.942 8.92 7.942ZM8.92 6.281C9.624 6.281 10.086 5.533 10.086 3.971C10.086 2.42 9.624 1.661 8.92 1.661C8.216 1.661 7.743 2.42 7.743 3.971C7.743 5.533 8.216 6.281 8.92 6.281Z"
-          transform="translate(9.07812 13.179)"
+          fill='var(--color-primary)'
+          fillrule='evenodd'
+          cliprule='evenodd'
+          d='M3.476 7.821V0.121H0.363L-1.79291e-07 1.683H1.54V7.821H3.476ZM8.92 7.942C6.951 7.942 5.785 6.347 5.785 3.971C5.785 1.584 6.951 -9.91821e-08 8.92 -9.91821e-08C10.878 -9.91821e-08 12.022 1.584 12.022 3.971C12.022 6.347 10.878 7.942 8.92 7.942ZM8.92 6.281C9.624 6.281 10.086 5.533 10.086 3.971C10.086 2.42 9.624 1.661 8.92 1.661C8.216 1.661 7.743 2.42 7.743 3.971C7.743 5.533 8.216 6.281 8.92 6.281Z'
+          transform='translate(9.07812 13.179)'
         ></path>
       </FastForward>
       <Listen>Listen</Listen>
       <StatusBar />
-      <VolumeIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <VolumeIcon xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
         <path
-          fill="var(--color-primary)"
-          fillrule="evenodd"
-          cliprule="evenodd"
-          d="M5 6L11 0L13 0V20H11L5 14H0V6H5Z"
-          transform="translate(0 2)"
+          fill='var(--color-primary)'
+          fillrule='evenodd'
+          cliprule='evenodd'
+          d='M5 6L11 0L13 0V20H11L5 14H0V6H5Z'
+          transform='translate(0 2)'
         ></path>
         <path
-          fill="var(--color-primary)"
-          fillrule="evenodd"
-          cliprule="evenodd"
-          d="M0 0C1.65685 0 3 1.34315 3 3C3 4.65685 1.65685 6 0 6L0 0Z"
-          transform="translate(16 9)"
+          fill='var(--color-primary)'
+          fillrule='evenodd'
+          cliprule='evenodd'
+          d='M0 0C1.65685 0 3 1.34315 3 3C3 4.65685 1.65685 6 0 6L0 0Z'
+          transform='translate(16 9)'
         ></path>
         <path
-          fill="var(--color-primary)"
-          d="M0.532837 0.0350647L0 0V1.99994L0.462067 2.0351C3.55962 2.27072 6 4.85941 6 8.01759C6 11.1758 3.55962 13.7645 0.462067 14.0001L0 14.0352V16.0352L0.532837 16.0001C4.70294 15.7256 8 12.2569 8 8.01759C8 3.77834 4.70294 0.309555 0.532837 0.0350647Z"
-          transform="translate(16 3.98241)"
+          fill='var(--color-primary)'
+          d='M0.532837 0.0350647L0 0V1.99994L0.462067 2.0351C3.55962 2.27072 6 4.85941 6 8.01759C6 11.1758 3.55962 13.7645 0.462067 14.0001L0 14.0352V16.0352L0.532837 16.0001C4.70294 15.7256 8 12.2569 8 8.01759C8 3.77834 4.70294 0.309555 0.532837 0.0350647Z'
+          transform='translate(16 3.98241)'
         ></path>
       </VolumeIcon>
     </Wrapper>
-  );
+  )
 }
 
-export default PlayBar;
+export default PlayBar
 
 const Wrapper = styled.div`
-  display: grid;
+  display: none;
+  /* display: grid; */
   grid-template-columns: 1fr 1fr 1fr 1fr 30fr 1fr;
   max-height: 3rem;
   margin-block-end: 3rem;
@@ -87,7 +88,11 @@ const Wrapper = styled.div`
   margin-inline: auto;
   align-items: center;
   gap: 1rem;
-`;
+
+  @media (min-width: 900px) {
+    display: grid;
+  }
+`
 const Listen = styled.span`
   display: block;
   text-transform: uppercase;
@@ -95,23 +100,23 @@ const Listen = styled.span`
   font-weight: 500;
   text-align: center;
   font-size: 0.85rem;
-`;
+`
 const Rewind = styled.svg`
   width: 30px;
-`;
+`
 const Play = styled.svg`
   width: 50px;
-`;
+`
 const FastForward = styled.svg`
   width: 30px;
-`;
+`
 const StatusBar = styled.div`
   /* flex: 5; */
   width: 100%;
   height: 2px;
   background-color: var(--color-primary);
   border-radius: 9999px;
-`;
+`
 const VolumeIcon = styled.svg`
   width: 30px;
-`;
+`
--- components/imageGrid/ImageGrid.js
+++ components/imageGrid/ImageGrid.js
@@ -1,7 +1,7 @@
-import styled from "styled-components";
-import CardWithBodyImage from "../ImageCards/CardWithBodyImage";
-import CardWithOptionalImage from "../ImageCards/CardWithOptionalImage";
-import ImageCardCenteredCopy from "../ImageCards/ImageCardCenteredCopy";
+import styled from 'styled-components'
+import CardWithBodyImage from '../ImageCards/CardWithBodyImage'
+import CardWithOptionalImage from '../ImageCards/CardWithOptionalImage'
+import ImageCardCenteredCopy from '../ImageCards/ImageCardCenteredCopy'
 
 function ImageGrid() {
   return (
@@ -9,75 +9,69 @@ function ImageGrid() {
       <GridWrapper>
         <GridArea1>
           <ImageCardCenteredCopy
-            backgroundImage="https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-1.png"
-            header=""
-            copy="Published by The New York Times, the project demonstrates how
-          slavery&nbsp;and&nbsp;its legacy have shaped modern America."
-            imageSrc="https://mwcm.nyt.com/dam/LP/brand/1619/img/The-1619.png"
+            backgroundImage='https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-1.png'
+            header=''
+            copy='Published by The New York Times, the project demonstrates how
+          slavery&nbsp;and&nbsp;its legacy have shaped modern America.'
+            imageSrc='https://mwcm.nyt.com/dam/LP/brand/1619/img/The-1619.png'
           />
           <CardWithBodyImage
-            imageSrc="https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-2_V2021.png"
-            alt="The 1619 Project Book Cover"
-            backgroundColor="hsla(212, 23%, 56%, 1);"
-            copy="A new book expands on the original reporting with new essays and poems
-            that illuminate slavery’s role in present-day America."
+            imageSrc='https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-2_V2021.png'
+            alt='The 1619 Project Book Cover'
+            backgroundColor='hsla(212, 23%, 56%, 1);'
+            copy='A new book expands on the original reporting with new essays and poems
+            that illuminate slavery’s role in present-day America.'
           />
         </GridArea1>
         <GridArea1>
           <CardWithBodyImage
-            imageSrc="https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-3.png"
-            alt="Mobile phone on NYT Website"
-            backgroundColor="hsla(41, 17%, 68%, 1.00)"
-            copy="A five-part audio narrative series examines the impact of slavery."
+            imageSrc='https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-3.png'
+            alt='Mobile phone on NYT Website'
+            backgroundColor='hsla(41, 17%, 68%, 1.00)'
+            copy='A five-part audio narrative series examines the impact of slavery.'
           />
 
           <ImageCardCenteredCopy
-            backgroundImage="https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-4_2.png"
-            header="A powerful Vision"
-            copy="The 1619 Project was intended to consider the connection of&nbsp;America’s past with its present. "
-            imageSrc="null"
+            backgroundImage='https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-4_2.png'
+            header='A powerful Vision'
+            copy='The 1619 Project was intended to consider the connection of&nbsp;America’s past with its present. '
+            imageSrc='null'
           />
         </GridArea1>
         <GridArea2>
-          <GridChild>
-            <CardWithOptionalImage
-              imageSrc="https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-2.png"
-              alt="Mobile phone on NYT Website"
-              backgroundColor="hsla(211, 11%, 67%, 1.00)"
-              copy="A special curriculum gives students a fuller understanding of slavery."
-              buttonCopy="Learn More"
-            />
-          </GridChild>
-          <GridChild>
-            <CardWithOptionalImage
-              backgroundColor="hsla(211, 20%, 64%, 1.00)"
-              copy="Show your support with special edition merchandise from our store"
-              cardHeight="190px"
-              buttonCopy="Shop"
-            />
-          </GridChild>
-          <GridChild>
-            <CardWithOptionalImage
-              backgroundColor="hsla(41, 26%, 63%, 1.00)"
-              copy="Honoring an important anniversary"
-              cardHeight="190px"
-              buttonCopy="Learn More"
-            />
-          </GridChild>
+          <CardWithOptionalImage
+            imageSrc='https://mwcm.nyt.com/dam/LP/brand/1619/img/Block-2.png'
+            alt='Mobile phone on NYT Website'
+            backgroundColor='hsla(211, 11%, 67%, 1.00)'
+            copy='A special curriculum gives students a fuller understanding of slavery.'
+            buttonCopy='Learn More'
+          />
+
+          <CardWithOptionalImage
+            backgroundColor='hsla(211, 20%, 64%, 1.00)'
+            copy='Show your support with special edition merchandise from our store'
+            cardHeight='190px'
+            buttonCopy='Shop'
+          />
+
+          <CardWithOptionalImage
+            backgroundColor='hsla(41, 26%, 63%, 1.00)'
+            copy='Honoring an important anniversary'
+            cardHeight='190px'
+            buttonCopy='Learn More'
+          />
         </GridArea2>
       </GridWrapper>
     </>
-  );
+  )
 }
 
-export default ImageGrid;
+export default ImageGrid
 
 const GridWrapper = styled.div`
   display: grid;
   gap: 0.85rem;
-  /* max-width: 1340px; */
-  /* margin-inline: auto; */
-`;
+`
 
 const GridArea1 = styled.div`
   display: flex;
@@ -92,23 +86,24 @@ const GridArea1 = styled.div`
     flex-direction: row;
     gap: 0.85rem;
   }
-`;
+`
 
 const GridArea2 = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  margin-inline: auto;
+  --min-column-width: min(320px, 100%);
+  display: grid;
+  padding: 16px;
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(var(--min-column-width), 1fr)
+  );
+  gap: 16px;
+  padding: 16px;
   &:first-child {
     margin-top: 4rem;
   }
 
-  @media (min-width: 768px) {
+  /* @media (min-width: 768px) {
     flex-direction: row;
     gap: 0.85rem;
-  }
-`;
-
-const GridChild = styled.div`
-  flex: 1 0 auto;
-`;
+  } */
+`
